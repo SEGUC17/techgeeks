33fee12b4ceec9a2ab8a247382b2886360f53c76
omarahmed411996 <omar.ahmed-ibrahim@student.guc.edu.eg>
2017-04-01 13:59:54 +0200

    reviews route

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+var express = require('express');
 var mongoose = require('mongoose');
-var router = express.Router();
-mongoose.connect("mongodb://localhost:27017/miniproject");
-//mongoose.connect("mongodb://localhost:27017/portofolio");
+var bodyParser = require('body-parser');
+
+
+mongoose.connect("mongodb://localhost:27017/techgeeks");
 
 var app = express();
 
-app.set("view engine", "ejs");
-
 //REQUIRE MODELS
+var Gyms = require("./models/gymmodel");
+var uploadPhoto = require("./models/photos");
+var reviews = require("./models/rating");
 
 //APP CONFIG
 app.set("view engines", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // ROUTES
-//app.use(require("./routes/reviews.js"));
-app.get("/reviews", function(req,res){
-    res.render("reviews.ejs");
-});
+app.use(require("./routes/homepage.js"));
+app.use(require("./routes/compare.js"));
+app.use(require("./routes/addgyms.js"));
+app.use(require("./routes/uploadPhoto.js"));
+app.use(require("./routes/reviews.js"));
+
 // SERVER LISTENING
 app.listen(3000, function() {
     console.log("Server is Running on Port 3000");
+});
+
+
+app.listen('3000', function(){
+console.log(" local host 3000");
+
 });
