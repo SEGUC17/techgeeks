acd6ab3bacde3f0b7ecb939701885291016f6df9
Eyad <eyad.amin@student.guc.edu.eg>
2017-04-01 11:46:24 +0200

    master updated

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require('mongoose');
+var router = express.Router();
+mongoose.connect("mongodb://localhost:27017/techgeeks");
+
+var app = express();
+
+app.set("view engine", "ejs");
+
+//REQUIRE MODELS
+var Gyms = require("./models/gymmodel");
+var uploadPhoto = require("./models/photos")
+    //APP CONFIG
+app.set("view engines", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// ROUTES
+app.use(require("./routes/homepage.js"));
+app.use(require("./routes/compare.js"));
+app.use(require("./routes/addgyms.js"));
+app.use(require("./routes/uploadPhoto.js"));
+
+//app.use(require("./routes/comparisonpage.js"));
+
+// SERVER LISTENING
+app.listen(3000, function() {
+    console.log("Server is Running on Port 3000");
+});
---
+++ models/gymmodel.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var GymSchema = new Schema({
+    Name: {
+        type: String,
+        unique: true
+
+    },
+    Location: {
+        type: String,
+        unique: true
+    },
+    Email: {
+        type: String,
+        unique: true
+    },
+    Telephone: {
+        type: Number,
+        unique: true
+
+    },
+    Openinghours: {
+        type: String,
+    },
+    Reviews: {
+        type: String,
+    },
+    Ratings: {
+        type: Number,
+    },
+});
+
+var Gym = mongoose.model("Gym", GymSchema);
+
+module.exports = Gym;
