ff6836bac3726525ddd22f02f83ed93ab9ceee0a
LinaSalah <lina.abdelmoaaty@student.guc.edu.eg>
2017-04-02 22:06:20 +0200

    gyminfo

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require('mongoose');
+var mysql = require('mysql');
 var router = express.Router();
+
 mongoose.connect("mongodb://localhost:27017/techgeeks");
 
 var app = express();
@@ -27,3 +29,10 @@ app.use(require("./routes/gyminfo.js"));
 app.listen(3000, function() {
     console.log("Server is Running on Port 3000");
 });
+
+console.log(Gyms);
+   for (var i in Gyms) {
+      console.log(i);
+   }
+
+   
