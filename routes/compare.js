1fe1d403dc57d25a38bed86e4f052d43f0f922ba
yasminemohamed <yasmiine.mohamed95@gmail.com>
2017-04-04 23:46:42 +0200

    Add files via upload

--- routes/compare.js
+++ routes/compare.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var router = express.Router();
 var bodyParser = require("body-parser");
+
+
 //REQUIRE Gym MODEL
 var Gyms = require("../models/gymmodel")
-router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get("/compare", function(req, res) {
     res.render("compare.ejs");
@@ -14,18 +16,36 @@ router.get("/comparisonfailed", function(req, res) {
 });
 
 router.post('/compare', (function(req, res) {
-    Gyms.findOne({ Name: req.body.name1, Name: req.body.name2 }, function(err, gyms) {
+    
+ var Name = req.body.name1
+ var Name2 = req.body.Name2
+    Gyms.find({ Name: Name }, function(err, gyms) {
         if (err) {
             res.send(err.message);
-            res.render("comparisonfailed.ejs");
-        }
-        if (gyms) {
-            res.redirect("/comparisonpage");
         } else {
-            console.log("error");
-            res.render("comparisonfailed.ejs");
+            console.log(Name);
+            console.log("rendering");
+            res.render('comparisonpage.ejs', { x: gyms });
         }
-    })
+    });
+
 }));
 
+
+
+
 module.exports = router;
+
+// Gyms.findOne({ Name: req.body.name1, Name: req.body.name2 }, function(err, gyms) {
+//         if (err) {
+//             res.send(err.message);
+//             res.render("comparisonfailed.ejs");
+//         }
+//         if (gyms) {
+//             res.redirect("/comparisonpage");
+//         } else {
+             
+//             console.log("error");
+//             res.render("comparisonfailed.ejs");
+//         }
+//     });
--- routes/comparisonpage.js
+++ routes/comparisonpage.js
@@ -1,17 +1,27 @@
 var express = require("express");
 var router = express.Router();
 var bodyParser = require("body-parser");
+var document = require("document")
 //REQUIRE Gym MODEL
 var Gyms = require("../models/gymmodel")
-router.use(bodyParser.urlencoded({ extended: false }));
-router.get('/comparisonpage', function(req, res) {
-    Gyms.find({ Name: 'Golds gym' }, function(err, gyms) {
-        if (err) {
-            res.send(err.message);
-        } else {
-            console.log("rendering");
-            res.render('comparisonpage', { x: gyms });
-        }
-    });
+router.use(bodyParser.urlencoded({ extended: true }));
+
+
+router.get("/comparisonpage", function(req, res) {
+    res.render("comparisonpage.ejs");
 });
+
+// router.get('/compare', function(req, res) {
+//     var Name = req.body.name1
+//     Gyms.find({ Name: Name }, function(err, gyms) {
+//         if (err) {
+//             res.send(err.message);
+//         } else {
+//             console.log(Name);
+//             console.log("rendering");
+//             res.render('comparisonpage', { x: gyms });
+//         }
+//     });
+// });
+
 module.exports = router;
