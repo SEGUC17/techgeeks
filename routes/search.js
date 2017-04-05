732dfd6e0eeacc6a6d0678b0d2dd96cc92abd1ac
eyadamin <eyad.amin@student.guc.edu.eg>
2017-04-05 14:02:30 +0200

    search for gyms upload
    search for gyms upload

--- routes/search.js
+++ routes/search.js
@@ -6,12 +6,14 @@ var Gyms = require("../models/gymmodel")
 router.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/search", function(req, res) {
-    res.render("search.ejs");
+    Gyms.find({ Name: req.body.name }, function(err, gyms) {
+        if (err) {
+            res.send(err.message);
+        } else {
+            console.log("rendering");
+            res.render('search', { x: gyms });
+        }
+    });
 });
-
-
-
-
-
 
 module.exports = router;
