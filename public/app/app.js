deec7af35d56c551a190f414827739c7be2d3970
LinaSalah <lina.abdelmoaaty@student.guc.edu.eg>
2017-04-22 15:03:15 +0200

    sprint 2 trial

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['appRoutes', 'reserveController', 'compareController'])
+angular.module('app', ['appRoutes', 'reserveController', 'compareController','gyminfoController'])
 
 
 .config(function() {
