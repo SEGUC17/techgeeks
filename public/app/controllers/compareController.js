f8fc4e02ebb525fe5ac9252e87c03394f1a00c68
Eyad <eyad.amin@studet.guc.edu.eg>
2017-04-22 15:10:13 +0200

    method read

--- public/app/controllers/compareController.js
+++ public/app/controllers/compareController.js
@@ -1,14 +1,9 @@
-console.log("testing compare controller");
 angular.module('compareController', [])
-    .controller('compareController', function($scope, $http, gyms) {
-        var self = this;
-        console.log("11")
-        this.compareGyms = () => {
-            $http.post("http://localhost:3000/routes/compare", self.data)
-                .then(function(response) {
-                    $scope.content = response.data;
-                    console.log(response.data);
-                })
-        }
+    .controller('compareController', function($http) {
+        this.compareGyms = function(data) {
+            console.log("form submitted");
+            console.log(this.data);
 
+            $http.post('/http://localhost:3000/routes/compare', this.data);
+        }
     });
--- public/app/controllers/reserveController.js
+++ public/app/controllers/reserveController.js
@@ -1,13 +1,9 @@
 angular.module('reserveController', [])
-.controller('reserveController', function($http){
-  this.clientReservation = function(reserveData){
-       console.log("form submitted");
-        console.log(this.reserveData);
+    .controller('reserveController', function($http) {
+        this.clientReservation = function(reserveData) {
+            console.log("form submitted");
+            console.log(this.reserveData);
 
-        $http.post('/http://localhost:3000/reserve', this.reserveData);
-   }
-});
-
-
-//http://localhost:3000/api/users
-//router.post('/reserve', (function(req, res) {
+            $http.post('/http://localhost:3000/routes/reserve', this.reserveData);
+        }
+    });
