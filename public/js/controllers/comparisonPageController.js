7fdfda9d0b2748888e5573e267875d5f92f7cb6b
Eyad <eyad.amin@studet.guc.edu.eg>
2017-04-23 16:14:39 +0200

    :sparkles: user can compare without logging in

--- public/js/controllers/comparisonPageController.js
+++ public/js/controllers/comparisonPageController.js
@@ -1,25 +1,20 @@
 var comparisonPageController = function($scope, $location, factory, $http) {
-    var isLoggedIn = factory.isLoggedIn();
+    $scope.gymComparison = factory.getGymComparison();
 
-    if (!isLoggedIn) {
-        $location.path('/login');
-    } else {
-        $scope.gymComparison = factory.getGymComparison();
+    $scope.gym1 = $scope.gymComparison.gym1;
+    $scope.gym2 = $scope.gymComparison.gym2;
 
-        $scope.gym1 = $scope.gymComparison.gym1;
-        $scope.gym2 = $scope.gymComparison.gym2;
-
-        $scope.reserve = function(gymName) {
-            factory.reserve(gymName)
-                .then(function(data) {
-                    alert('Reservation created!');
-                })
-                .catch(function(error) {
-                    alert(error.data.error);
-                });
-        }
+    $scope.reserve = function(gymName) {
+        factory.reserve(gymName)
+            .then(function(data) {
+                alert('Reservation created!');
+            })
+            .catch(function(error) {
+                alert(error.data.error);
+            });
     }
 }
+
 
 comparisonPageController.$inject = ['$scope', '$location', 'factory', '$http'];
 App.controller('comparisonPageController', comparisonPageController);
