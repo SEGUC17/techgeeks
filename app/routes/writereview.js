f75ad993f6bb747e52b049cc9818e3f53ac958ee
Eyad <eyad.amin@studet.guc.edu.eg>
2017-04-29 11:46:51 +0200

    #66 conditions

--- app/routes/writereview.js
+++ app/routes/writereview.js
@@ -8,86 +8,46 @@ var Review = require('../models/reviewmodel');
 //REQUIRE Clients MODEL
 var Clients = require('../models/clientmodel');
 //WRITING REVIEWS ON THE GYMS
-
-
-
 router.post('/writereview', (function(req, res) {
-    Review.findOne({
-        $and: [{ gname: req.body.gymName }, { username: req.body.username }]
-
-    }, function(err, review) {
+    Gyms.findOne({ Name: req.body.gymName }, function(err, gym) {
         if (err) {
-            return res.status(500).json({
+            res.status(500).json({
                 error: 'Internal server error',
                 data: null
             });
-        } else if (review) {
-            return res.status(500).json({
-                error: "You already subitted your review",
-                data: null
-            });
-        } else {
 
-
-            Gyms.findOne({
-                Name: req.body.gymName
-            }, function(err, gym) {
+        } else if (gym) {
+            Clients.findOne({ username: req.body.username }, function(err, client) {
                 if (err) {
-                    return res.status(500).json({
+                    console.log(err);
+                    res.status(500).json({
                         error: 'Interal server error',
                         data: null
                     });
+                } else if (client) {
+
+                    var Review = require('../models/reviewmodel');
+
+                    Review.create({ Username: req.body.username, Name: req.body.gymName, Reviews: req.body.review, Ratings: req.body.rating },
+                        function(err, Review) {
+                            if (err) {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: 'Interal server error',
+                                    data: null
+                                });
+                            } else {
+                                return res.json({
+                                    error: null,
+                                    data: Review
+                                });
+                            }
+                        })
                 }
-
-                if (gym) {
-                    Clients.findOne({ username: req.body.username }, function(err, client) {
-                        if (err) {
-                            return res.status(500).json({
-                                error: 'Interal server error',
-                                data: null
-                            });
-                        }
-
-                        if (client) {
-                            Review.create({
-                                gname: req.body.gymName,
-                                username: req.body.username
-                            }, function(err, review) {
-                                if (err) {
-                                    return res.status(500).json({
-                                        error: 'Interal server error',
-                                        data: null
-                                    });
-                                } else {
-                                    return res.json({
-                                        error: null,
-                                        data: review
-                                        
-                                    });
-                                }
-                            })
-
-                        } else {
-                            return res.status(404).json({
-                                error: 'User not found',
-                                data: null
-                            });
-                        }
-                    });
-                } else {
-                    return res.status(404).json({
-                        error: 'Gym not found',
-                        data: null
-                    });
-                }
-            });
+            })
 
         }
-    })
+    });
 }));
 
-
-
 module.exports = router;
-
-
