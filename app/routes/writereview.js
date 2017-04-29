1ae2b6cb9dcc951899fd15172ed22db04870be93
eyadamin <eyad.amin@student.guc.edu.eg>
2017-04-29 13:33:20 +0200

    Update writereview.js

--- app/routes/writereview.js
+++ app/routes/writereview.js
@@ -1,43 +1,5 @@
 var express = require("express");
 var router = express.Router();
-//REQUIRE Gym MODEL
-var Gyms = require("../models/gymmodel");
-// COMPARING BETWEEN TWO GYMS ACCORDING TO THEIR NAMES
-router.post('/compare', function(req, res) {
-    var gym1 = req.body.gym1;
-    var gym2 = req.body.gym2;
-
-    Gyms.find({
-        $and: [{
-            $or: [{ Name: gym1 }, { Name: gym2 }]
-        }]
-    }, function(err, gyms) {
-        if (err) {
-            return res.status(500).json({
-                error: 'Internal server error',
-                data: null
-            });
-        }
-            
-        if (gyms.length === 0  ||  gyms.length === 1)  {   
-            return res.status(404).json({
-                error: 'Gyms not found',
-                data: null
-            });
-        } else {
-            return res.json({
-                error: null,
-                data: gyms
-            });
-         }
-    });
-
-});
-
-module.exports = router;
-
-var express = require("express");
-var router = express.Router();
 var mongoose = require('mongoose');
 //REQUIRE Gym MODEL
 var Gyms = require('../models/gymmodel');
